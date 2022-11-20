b48664ee599ef9c7e41e49b989228a3a91ecec76
Rahul  Parihar <65505794+rahul72parihar@users.noreply.github.com>
2022-11-20 15:13:57 +0530

    push all data

---
+++ data.js
@@ -0,0 +1,32 @@
+export const postData =[
+    {
+        imgSrc:"https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+        date:"12/1/2001",
+        heading:"Post Heading",
+        paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse nobis deleniti minima corrupti alias sapiente possimus aperiam fugit nam doloribus ipsum corporis, nisi at. Ducimus itaque iusto ipsam dolor?",
+    },
+    {
+        imgSrc:"https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+        date:"13/1/2001",
+        heading:"Post Heading",
+        paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse nobis deleniti minima corrupti alias sapiente possimus aperiam fugit nam doloribus ipsum corporis, nisi at. Ducimus itaque iusto ipsam dolor?",
+    },
+    {
+        imgSrc:"https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+        date:"12/1/2001",
+        heading:"Post Heading",
+        paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse nobis deleniti minima corrupti alias sapiente possimus aperiam fugit nam doloribus ipsum corporis, nisi at. Ducimus itaque iusto ipsam dolor?",
+    },
+    {
+        imgSrc:"https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+        date:"12/1/2001",
+        heading:"Post Heading",
+        paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse nobis deleniti minima corrupti alias sapiente possimus aperiam fugit nam doloribus ipsum corporis, nisi at. Ducimus itaque iusto ipsam dolor?",
+    },
+    {
+        imgSrc:"https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+        date:"12/1/2001",
+        heading:"Post Heading",
+        paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus esse nobis deleniti minima corrupti alias sapiente possimus aperiam fugit nam doloribus ipsum corporis, nisi at. Ducimus itaque iusto ipsam dolor?",
+    },
+]
--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // banner open script
+import { postData } from "./data.js"
 const bannerEl = document.getElementById("banner-el")
 console.log(bannerEl)
 bannerEl.addEventListener('click',function(){
     console.log("clicked")
     window.open('#', '_blank');
+})
+
+// RENDER 
+
+const mainEl = document.getElementById("main-el")
+let currMax = 3;
+function renderMain(){
+    let toRender = ``
+    let i
+    for(i = 0; i<currMax && i<postData.length; i++){
+        toRender+=  
+        `
+        <div class="post clickable" data-uuid =${i}>
+        <img class="main-image" src=${postData[i].imgSrc} alt="post image">
+        <p class="main-date">${postData[i].date}</p>
+        <p class="main-heading">${postData[i].heading}</p>
+        <p class="main-para">${postData[i].paragraph}</p>
+        </div>
+        `
+    }
+    if(i<postData.length){
+        toRender+= 
+            `
+                <div id="more-post">
+                    <img id = "more-btn" class="more-image clickable" src="images/plus.png" alt="More Post">
+                    <p id ="more-head" class="more-heading">View More</p>
+                </div>
+            `
+    }
+    mainEl.innerHTML=toRender;
+}
+//render max 3 items by default
+renderMain()
+
+
+// MOREBTN
+document.addEventListener('click',function(e){
+    if(e.target.classList.contains('more-image')){
+        currMax+=3;
+        renderMain()
+    }
+    else if(e.target.classList.contains('post')){
+        console.log(e.target.dataset.uuid)
+    }
+})
+// post opening
+document.addEventListener('click',function(e){
+    if(e.target.classList.contains('more-image')){
+        currMax+=3;
+    renderMain()
+    }
 })
