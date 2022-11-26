90a273aafb2a7d9803c071f8cfc4c64b0c35b157
Rahul  Parihar <65505794+rahul72parihar@users.noreply.github.com>
2022-11-27 00:00:01 +0530

    made layout changes to icon buttons

--- index.js
+++ index.js
@@ -18,27 +18,24 @@ function renderMain(){
     for(i = 0; i<currMax && i<postData.length; i++){
         toRender+=  
         `
-        
         <div class="post">
             <a href= ${postData[i].link}>
                 <img class="main-image" src=${postData[i].imgSrc} alt="post image">
             </a>
             <p class="main-date">${postData[i].date}</p>
             <p class="main-heading">${postData[i].heading}</p>
-            <p class="main-heading">
+            <div class="main-heading icon-div">
                 <a href= "${postData[i].link}" target=_blank"">
                     <i class="fa fa-arrow-up icon">
                         <span class="github-text">LIVE SITE</span>
                     </i>
                 </a>
-            </p>
-            <p class="main-heading">
                 <a href= "${postData[i].git}" target="_blank">
                     <i class="fa fa-github icon">
                         <span class="github-text">GITHUB CODE</span>
                     </i>
                 </a>
-            </p>
+            </div>
             
             <p class="main-para">${postData[i].paragraph}</p>
         </div>
