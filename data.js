b886f4e0a21e728066a1ad4001d7f28ebc1ff66e
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-04-04 19:05:11 +0530

    push 3 projects

--- data.js
+++ data.js
@@ -1,66 +1,91 @@
-export const postData =[
-    {
-        imgSrc:"images/project/movie.PNG",
-        date:"Dec 2022",
-        heading:"Movie WatchList Maker",
-        paragraph:"This app helps make you your watchlist and make use of IMdB API form fetching data and uses local storage for saving your watchlist. ADD/REMOVE movie from your watchlist.!!!",
-        link:"https://rahulp.me/WatchList/",
-        git: "https://github.com/rahul72parihar/WatchList",
-    },
-    {
-        imgSrc:"images/project/trivia.PNG",
-        date:"Dec 2022",
-        heading:"React APP Trivia",
-        paragraph:"Uses Trivia databaset to fetch question and use React to Render App",
-        link:"https://rahulp.me/trivia/",
-        git: "https://github.com/rahul72parihar/trivia",
-    },
-    {
-        imgSrc:"images/project/songRanker.PNG",
-        date:"Nov 2022",
-        heading:"Song Ranker App",
-        paragraph:"This app ranked 32 song of 2022 so you can see the top song and the champion and your choice",
-        link:"https://rahulp.me/songRanker/",
-        git: "https://github.com/rahul72parihar/songRanker",
-    },
-    {
-        imgSrc:"images/project/personality.PNG",
-        date:"Nov 2022",
-        heading:"Personality Test",
-        paragraph:"This is a 16 Personality test made with the help of JS. This show your personality on basic of Myers Briggs Personality Indicators, and shows celebs with your personality too!!!",
-        link:"https://rahulp.me/Personality-test/",
-        git: "https://github.com/rahul72parihar/Personality-test",
-    },
-    {
-        imgSrc:"images/project/colorPicker.PNG",
-        date:"Nov 2022",
-        heading:"Color Scheme (Extension)",
-        paragraph:"Pick Color with the help of an extention taking color from and API",
-        link:"https://rahulp.me/Color-Scheme-Picker/",
-        git: "https://github.com/rahul72parihar/Color-Scheme-Picker.git",
-    },
-    {
-        imgSrc:"images/project/instagram.PNG",
-        date:"Aug 2022",
-        heading:"Instagram Clone (CSS Project)",
-        paragraph:"Clone of Instagram Website made with the help of css",
-        link:"https://rahulp.me/Oldstagram/",
-        git: "https://github.com/rahul72parihar/Oldstagram",
-    },
-    {
-        imgSrc:"images/project/conversion.PNG",
-        date:"Jul 2022",
-        heading:"Unit Conversion (CHROME EXTENSION)",
-        paragraph:"This take your input in a chrome extension and display its convesion in 3 units, this is a chrome extension, the conversion is done and rendered using JS.",
-        link:"https://rahulp.me/Metric-Imperial-Unit-Conversion/",
-        git: "https://github.com/rahul72parihar/Metric-Imperial-Unit-Conversion",
-    },
-    {
-        imgSrc:"images/project/password.PNG",
-        date:"Jul 2022",
-        heading:"Password Generator",
-        paragraph:"This app make random password of 15 character of any aascii character",
-        link:"https://rahulp.me/password-generator/",
-        git: "https://github.com/rahul72parihar/password-generator",
-    },
-]
+export const postData = [
+  {
+    imgSrc: "images/project/ecommerce.PNG",
+    date: "Mar 2023",
+    heading: "Book Ecommerce(RPSTORE)",
+    paragraph:
+      "Ecommerce site with cart and authentication, Protected Routes, Context API",
+    link: "https://odinecommerce.netlify.app/home",
+    git: "https://github.com/rahul72parihar/OdinEcommerce",
+  },
+  {
+    imgSrc: "images/project/odintodo.PNG",
+    date: "Feb 2022",
+    heading: "TO-DO List",
+    paragraph:
+      "To do list PC responsive only Build In Vanilla js and webpack only!!!!",
+    link: "https://rahulp.me/OdinToDo/",
+    git: "https://github.com/rahul72parihar/OdinToDo",
+  },
+  {
+    imgSrc: "images/project/basewebpassword.PNG",
+    date: "Apr 2022",
+    heading: "BaseWeb Password Generator",
+    paragraph:
+      "Used Baseweb Library by Uber to create Password Generator. REACT BASEWEB",
+    link: "https://basewebpassword.netlify.app/",
+    git: "https://github.com/rahul72parihar/BaseWebPassword",
+  },
+  {
+    imgSrc: "images/project/trivia.PNG",
+    date: "Dec 2022",
+    heading: "React APP Trivia",
+    paragraph:
+      "Uses Trivia databaset to fetch question and use React to Render App",
+    link: "https://rahulp.me/trivia/",
+    git: "https://github.com/rahul72parihar/trivia",
+  },
+  {
+    imgSrc: "images/project/songRanker.PNG",
+    date: "Nov 2022",
+    heading: "Song Ranker App",
+    paragraph:
+      "This app ranked 32 song of 2022 so you can see the top song and the champion and your choice",
+    link: "https://rahulp.me/songRanker/",
+    git: "https://github.com/rahul72parihar/songRanker",
+  },
+  {
+    imgSrc: "images/project/personality.PNG",
+    date: "Nov 2022",
+    heading: "Personality Test",
+    paragraph:
+      "This is a 16 Personality test made with the help of JS. This show your personality on basic of Myers Briggs Personality Indicators, and shows celebs with your personality too!!!",
+    link: "https://rahulp.me/Personality-test/",
+    git: "https://github.com/rahul72parihar/Personality-test",
+  },
+  {
+    imgSrc: "images/project/colorPicker.PNG",
+    date: "Nov 2022",
+    heading: "Color Scheme (Extension)",
+    paragraph:
+      "Pick Color with the help of an extention taking color from and API",
+    link: "https://rahulp.me/Color-Scheme-Picker/",
+    git: "https://github.com/rahul72parihar/Color-Scheme-Picker.git",
+  },
+  {
+    imgSrc: "images/project/instagram.PNG",
+    date: "Aug 2022",
+    heading: "Instagram Clone (CSS Project)",
+    paragraph: "Clone of Instagram Website made with the help of css",
+    link: "https://rahulp.me/Oldstagram/",
+    git: "https://github.com/rahul72parihar/Oldstagram",
+  },
+  {
+    imgSrc: "images/project/conversion.PNG",
+    date: "Jul 2022",
+    heading: "Unit Conversion (CHROME EXTENSION)",
+    paragraph:
+      "This take your input in a chrome extension and display its convesion in 3 units, this is a chrome extension, the conversion is done and rendered using JS.",
+    link: "https://rahulp.me/Metric-Imperial-Unit-Conversion/",
+    git: "https://github.com/rahul72parihar/Metric-Imperial-Unit-Conversion",
+  },
+  {
+    imgSrc: "images/project/password.PNG",
+    date: "Jul 2022",
+    heading: "Password Generator",
+    paragraph:
+      "This app make random password of 15 character of any aascii character",
+    link: "https://rahulp.me/password-generator/",
+    git: "https://github.com/rahul72parihar/password-generator",
+  },
+];
